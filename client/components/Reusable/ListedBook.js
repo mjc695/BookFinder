793ca57a50b86e7576dacd2fc5bb5ac50baaed4a
Monferd Collin <monferd.j.collin@gmail.com>
2019-01-10 15:14:45 -0500

    finishing css

--- client/components/Reusable/ListedBook.js
+++ client/components/Reusable/ListedBook.js
@@ -4,14 +4,18 @@ import {NavLink} from 'react-router-dom'
 const ListedBook = props => {
   const {book, addBook} = props
   return (
-    <div onClick={() => addBook(book)}>
+    <div className="searchResults" onClick={() => addBook(book)}>
       <NavLink to="/singleBook">
         <div>
-          <img src={book.cover && book.cover.small} />
-          <p className="SearchTitles">{book.title}</p>
-          {book.author_name ? (
-            <p className="SearchAuthors">by {book.author_name}</p>
-          ) : null}
+          <img src={book.cover ? book.cover.small : 'noImage.png'} />
+          <div>
+            <p className="SearchTitles">{book.title}</p>
+            {book.author_name ? (
+              <p className="SearchAuthors">by {book.author_name}</p>
+            ) : (
+              <p>No author available</p>
+            )}
+          </div>
         </div>
       </NavLink>
     </div>
